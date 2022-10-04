c6b5a3eb051247c30c9f5ff32ab7b015408146cd
MarioEduardo <eduardoat44@gmail.com>
2022-10-03 20:59:21 -0500

    Sesion 4 - Se agrega objeto gods y get con nombres

--- backend-fundamentals/Sesion 4/app.js
+++ backend-fundamentals/Sesion 4/app.js
@@ -3,10 +3,25 @@ const app = express();
 
 const PORT = 3000;
 
+const gods = {
+    Zeus:{live:'Olympus',symbol: 'Thunderbolt'},
+    Hades:{live:'Underworld',symbol:'Cornucopia'}
+}
+
 app.get('/',(req,res) =>{
     res.json({
         msg: 'Get de Proyecto'
     })
+});
+
+app.get('/gods',(req,res)=>{
+    res.json(gods);
+});
+
+app.get('/gods/:name',(req,res)=>{
+    //const name = req.params.name;
+    const {name} = req.params;
+    res.json(gods[name]);
 });
 
 app.listen(PORT,() =>{
